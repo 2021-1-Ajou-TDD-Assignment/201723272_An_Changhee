50c419bd70b2024c5e3d09647d14c5f2e153bb89
leaping <leaping96@ajou.ac.kr>
2021-04-12 17:56:18 +0900

    The forth test-1

--- src/test/BowlingTest.java
+++ src/test/BowlingTest.java
@@ -44,12 +44,22 @@ public class BowlingTest {
 		assertEquals(16,g.score());
 	}
 
+	
+	@Test
+	public void testOneStrike() {
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(16,0);
+		assertEquals(24,g.score());
+	}
+	
+	private void rollStrike() {
+		g.roll(10);
+	}
+	
 	private void rollSpare() {
 		g.roll(5);
 		g.roll(5);
-		
 	}
-	
-	
-	
 }
