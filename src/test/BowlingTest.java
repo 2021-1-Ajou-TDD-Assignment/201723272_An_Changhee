5881f30b707b2832576d41efce659593da1db8c1
leaping <leaping96@ajou.ac.kr>
2021-04-12 17:46:58 +0900

    The third test-3

--- src/test/BowlingTest.java
+++ src/test/BowlingTest.java
@@ -36,14 +36,20 @@ public class BowlingTest {
 		assertEquals(20,g.score());
 	}
 	
-	@Ignore("until we walk frame by frame")
+	
 	@Test public void testOneSpare() {
-		g.roll(5);
-		g.roll(5); // spare
+		rollSpare();
 		g.roll(3);
 		rollMany(17,0);
 		assertEquals(16,g.score());
 	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+		
+	}
+	
 	
 	
 }
